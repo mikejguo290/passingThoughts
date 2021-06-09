afad604cad7cb8c15f0967b13780074c4ef2a516
mikejguo290 <mikejguo290@gmail.com>
2021-06-09 23:54:10 +0100

    4. store the value of input as state called text. create eventhandler for this and pass as prop to input.

--- src/AddThoughtForm.js
+++ src/AddThoughtForm.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { generateId, getNewExpirationTime } from './utilities';
 
 export function AddThoughtForm(props) {
+  // AddThoughtForm requires state to record user input and be able to pass it on as thought to addThought() call
+  // thought is if a function is passed to component, where is that component expected to get that data? should it store it itself?
+  const [text, setText ] = useState('');
+
+  const handleTextChange = (e) =>{
+    // whenever user inputs a value, this function gets called to update state text, which also updates the value of the input box explicitly.
+    const input = e.target.value;
+    setText(input);
+  }
+
   return (
     <form className="AddThoughtForm">
       <input
         type="text"
         aria-label="What's on your mind?"
         placeholder="What's on your mind?"
+        value={text}
+        onChange={handleTextChange}
       />
       <input type="submit" value="Add" />
     </form>
