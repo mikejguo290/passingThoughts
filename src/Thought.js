4a514bac7c870d9e8011654c1a2ceadd15f0c753
mikejguo290 <mikejguo290@gmail.com>
2021-06-10 16:17:24 +0100

    Rationalie for including 'thought' in useEffect's dependency array.

--- src/Thought.js
+++ src/Thought.js
@@ -11,10 +11,11 @@ export function Thought(props) {
 
   useEffect(()=>{
     // setTimeOut sets a timer which executes a piece of code once the timer expires.
-    // useEffect to run whenever the props.thought changes, can't see how it might change however? if thought was removed, wouldn't the component simply unmount?
-    // for each thoughts re rendering caused by change to thoughts in app's state, the thought passed down wouldn't be able to change. 
-    // maybe it's just good practice? 
-    // eslint says React Hook useEffect has a missing dependency:removeThought! why? 
+    // useEffect to run whenever the props.thought changes, whenenver thoughts state changes, i.e. additions or deleitions, 
+    // Thought will be passed updated props.thought and so the effect should be used on the updated thought props. targeting the right thought.id.
+    // even though i haven't managed to break the app with an empty dependency array []
+
+    // if any effect depends on props value, then it should be ran on each update re render. as well as on mount.
     const timeRemaining = thought.expiresAt - Date.now(); // more accurate than passing 15*1000 as second arg to setTimeOut()
     const timeoutID = setTimeout(()=>{ removeThought(thought.id) }, timeRemaining);
     return ()=> { clearTimeout(timeoutID)}
